37110f8f3acd5e154be5166930f4a36fbc59af79
shaoyongyang <shaoyongyang1993@gmail.com>
2016-02-23 17:20:19 +0800

    modify size

--- upload.go
+++ upload.go
@@ -13,10 +13,6 @@ import (
 	"time"
 )
 
-type Sizer interface {
-	Size() int64
-}
-
 type JsonData struct {
 	FileType string `json:"type"`
 	FileSize string `json:"size"`
@@ -38,6 +34,10 @@ func isDirExits(path string) bool {
 var DO_MAIN, LISTEN_PORT string
 var EXPAND []string
 
+type Sizer interface {
+	Size() int64
+}
+
 func upload(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if r.Method == "GET" {
@@ -52,6 +52,13 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		if ext == "noExt" {
 			return
 		}
+		//
+		fileSizer, _ := file.(Sizer)
+		if fileSizer.Size() > customfileSize {
+			io.WriteString(w, "maxFileSize "+strconv.FormatInt(customfileSize, 10)+" M")
+			return
+		}
+		fmt.Println(fileSizer.Size())
 		FINAL_PATH := "file/" + year + "/" + month + "/" + day + "/"
 		if !isDirExits(FINAL_PATH) {
 			os.MkdirAll(FINAL_PATH, 0700)
@@ -66,7 +73,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 			fileMSG := JsonData{ext, strconv.FormatInt(file.(Sizer).Size(), 10), DO_MAIN + FINAL_PATH}
 			data, err := json.Marshal(fileMSG)
 			io.WriteString(w, string(data))
-			fmt.Println("SaveFile:" + FINAL_PATH)
+			fmt.Println(r.Host + " SaveFile " + FINAL_PATH)
 		} else {
 			io.WriteString(w, "The FileExpand No Access!")
 		}
@@ -101,6 +108,8 @@ func fileName() string {
 	return year + month + day + hour + minute + second
 }
 
+var customfileSize int64
+
 func initIniFile() {
 	c, err := goconfig.LoadConfigFile("settings.ini")
 	checkErr(err)
@@ -110,6 +119,8 @@ func initIniFile() {
 	checkErr(err)
 	expand, err := c.GetValue(goconfig.DEFAULT_SECTION, "uploadType")
 	checkErr(err)
+	maxfileSize, err := c.Int64(goconfig.DEFAULT_SECTION, "uploadSize")
+	customfileSize = maxfileSize
 	DO_MAIN = domain
 	LISTEN_PORT = listenPort
 	EXPAND = strings.Split(expand, ",")
@@ -124,6 +135,7 @@ func checkErr(err error) {
 func main() {
 	initIniFile()
 	http.HandleFunc("/upload", upload)
+	http.Handle("/", http.FileServer(http.Dir("./")))
 	err := http.ListenAndServe(":"+LISTEN_PORT, nil)
 	if err != nil {
 		log.Fatal("listenAndServe: ", err)
